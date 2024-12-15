2982da374010ec2903116711ed810ede54dad858
LemonTEA <green.c.lemon@gmail.com>
2024-12-15 15:09:05 +0900

    スタイルを整える

--- src/ground/page/app.tsx
+++ src/ground/page/app.tsx
@@ -1,5 +1,6 @@
 // React MUI 関連
 import Paper from '@mui/material/Paper';
+import { ThemeProvider } from '@emotion/react';
 
 // React Router 関連
 import { HashRouter, Route, Routes } from 'react-router-dom';
@@ -9,21 +10,26 @@ import PaletteBar from './palette-bar';
 import PaletteList from './palette-list';
 import PaletteEdit from './palette-edit';
 
+// ページテーマ
+import theme from './theme';
+
 // アプリケーションのルートコンポーネント
 function App() {
 
   // レンダリングを行う
   return (
-    <Paper variant='outlined' elevation={0} sx={{height:'100%',borderRadius:0}}>
-      <HashRouter>
-        <Routes>
-          <Route path='/' element={<PaletteBar/>}>
-            <Route path='/' element={<PaletteList/>}/>
-            <Route path='/edit/:id' element={<PaletteEdit/>}/>
-          </Route>
-        </Routes>
-      </HashRouter>
-    </Paper>
+    <ThemeProvider theme={theme}>
+      <Paper variant='outlined' elevation={0} sx={{height:'100%',borderRadius:0}}>
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<PaletteBar/>}>
+              <Route path='/' element={<PaletteList/>}/>
+              <Route path='/edit/:id' element={<PaletteEdit/>}/>
+            </Route>
+          </Routes>
+        </HashRouter>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
